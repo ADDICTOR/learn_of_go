dcdfb1e5ed69de55bfe61fe532e1b145ddd9bf93
ADDICTOR <2894515992@qq.com>
2023-06-02 15:44:20 +0800

    [feature : new demo]

--- chapter2/boiling.go
+++ chapter2/boiling.go
@@ -8,7 +8,7 @@ const boilingF = 212.0
 func main() {
 	var f = boilingF
 	var c = (f - 32) * 5 / 9
-	fmt.Printf("boiling point = %g°F or %G°C\n", f, c)
+	fmt.Printf("boiling point = %g°F or %g°C\n", f, c)
 	// 输出
 	// boiling point = 212°F or 100°C
 }
